168633884b7f1aed543da09712b5bbf54989075d
unknown <you@example.com>
2022-04-01 00:14:03 +0200

    first commitlg

---
+++ cafeteria.sql
@@ -0,0 +1,240 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Apr 01, 2022 at 12:13 AM
+-- Server version: 10.4.22-MariaDB
+-- PHP Version: 7.4.27
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `cafeteria`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `categories`
+--
+
+CREATE TABLE `categories` (
+  `id` int(11) NOT NULL,
+  `category_name` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `categories`
+--
+
+INSERT INTO `categories` (`id`, `category_name`) VALUES
+(1, 'hot'),
+(2, 'cold'),
+(3, 'soft');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `oproduct`
+--
+
+CREATE TABLE `oproduct` (
+  `id` int(11) NOT NULL,
+  `product_id` int(11) NOT NULL,
+  `order_id` int(11) NOT NULL,
+  `amount` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `oproduct`
+--
+
+INSERT INTO `oproduct` (`id`, `product_id`, `order_id`, `amount`) VALUES
+(1, 4, 65, 2),
+(2, 3, 65, 3),
+(3, 8, 65, 2),
+(4, 4, 66, 2),
+(5, 3, 66, 3),
+(6, 8, 66, 2),
+(7, 4, 67, 3),
+(8, 3, 67, 5),
+(9, 3, 68, 2),
+(10, 4, 68, 1),
+(11, 3, 69, 2),
+(12, 8, 69, 3),
+(13, 3, 1, 2),
+(14, 4, 1, 4),
+(15, 8, 2, 2),
+(16, 4, 2, 2),
+(17, 12, 3, 3),
+(18, 4, 3, 3),
+(19, 4, 6, 1),
+(20, 8, 7, 2),
+(21, 4, 8, 2),
+(22, 8, 8, 3),
+(23, 12, 8, 3);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `orders`
+--
+
+CREATE TABLE `orders` (
+  `order_id` int(11) NOT NULL,
+  `total` int(11) NOT NULL,
+  `note` varchar(100) NOT NULL,
+  `room` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `date` date NOT NULL,
+  `staus` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `orders`
+--
+
+INSERT INTO `orders` (`order_id`, `total`, `note`, `room`, `user_id`, `date`, `staus`) VALUES
+(1, 134, 'ss', 123, 8, '2022-03-31', 'deliver'),
+(2, 74, 'hot', 123, 9, '2022-03-31', 'deliver'),
+(3, 150, 'ss', 55, 7, '2022-03-31', 'deliver'),
+(4, 0, '', 0, 0, '2022-03-31', 'processing'),
+(5, 0, '', 0, 0, '2022-03-31', 'processing'),
+(6, 30, '', 0, 0, '2022-03-31', 'processing'),
+(7, 14, '', 0, 0, '2022-03-31', 'processing'),
+(8, 141, 'hot', 23, 5, '2022-03-31', 'processing');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `products`
+--
+
+CREATE TABLE `products` (
+  `product_id` int(11) NOT NULL,
+  `product_name` varchar(100) NOT NULL,
+  `product_price` int(11) NOT NULL,
+  `category_id` int(11) NOT NULL,
+  `amount` int(11) NOT NULL,
+  `image` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `products`
+--
+
+INSERT INTO `products` (`product_id`, `product_name`, `product_price`, `category_id`, `amount`, `image`) VALUES
+(1, 'soft drink', 7, 1, 10, 'coffe2.jpg'),
+(4, 'french coffe', 30, 1, 10, 'images (1).jpg'),
+(8, 'tea', 7, 1, 10, 'coffe4.jpg'),
+(12, 'latte', 20, 3, 10, 'coffe.jpg');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `user_id` int(11) NOT NULL,
+  `user_name` varchar(100) NOT NULL,
+  `user_email` varchar(200) NOT NULL,
+  `user_password` varchar(100) NOT NULL,
+  `user_room` int(11) NOT NULL,
+  `user_ext` int(11) NOT NULL,
+  `role` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`user_id`, `user_name`, `user_email`, `user_password`, `user_room`, `user_ext`, `role`) VALUES
+(1, 'sandy', 'sandy@gmail.com', '123', 0, 1, 'admin'),
+(4, 'sandy maged', 'sandy@gmail.com', '123', 1, 5, 'user'),
+(5, 'sahar', 'sandy@gmail.com', '123', 0, 0, 'user'),
+(7, 'reem', 'sandy@gmail.com', '123', 20, 1, 'user'),
+(8, 'sahar', 'sandy@gmail.com', '123', 33, 2, 'user'),
+(9, 'peter', 'sandy@gmail.com', '123', 22, 1, 'user');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `categories`
+--
+ALTER TABLE `categories`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `oproduct`
+--
+ALTER TABLE `oproduct`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `orders`
+--
+ALTER TABLE `orders`
+  ADD PRIMARY KEY (`order_id`);
+
+--
+-- Indexes for table `products`
+--
+ALTER TABLE `products`
+  ADD PRIMARY KEY (`product_id`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`user_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `categories`
+--
+ALTER TABLE `categories`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `oproduct`
+--
+ALTER TABLE `oproduct`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=24;
+
+--
+-- AUTO_INCREMENT for table `orders`
+--
+ALTER TABLE `orders`
+  MODIFY `order_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
+--
+-- AUTO_INCREMENT for table `products`
+--
+ALTER TABLE `products`
+  MODIFY `product_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
